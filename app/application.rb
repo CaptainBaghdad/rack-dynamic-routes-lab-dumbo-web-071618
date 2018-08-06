64617f917fc6e12504c55c9b84b76dfd0ee158bb
Jason Pitts <juniorjr083@gmail.com>
2018-08-06 00:34:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,8 @@ class Application
       res = Rack::Response.new 
       if req.path.match(/items/)
       ans = req.params.split('/item/').last
-      the_item = Item.all.find {|ele|}
+      the_item = Item.all.find {|ele| ele.name == ans}
+      if
       res.write "#{ans}" 
         
      end 
